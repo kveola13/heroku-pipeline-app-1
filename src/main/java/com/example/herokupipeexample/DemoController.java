62285eea1d032aa5548b9145ad13985e7468894b
Glenn Bech <glenn.bech@gmail.com>
2018-10-07 21:34:33 +0200

    controller

--- src/main/java/com/example/herokupipeexample/DemoController.java
+++ src/main/java/com/example/herokupipeexample/DemoController.java
@@ -3,10 +3,10 @@ package com.example.herokupipeexample;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;s
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class DemoController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
