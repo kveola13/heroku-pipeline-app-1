5b5b86fe47e27e5ee0060a89858519300329383d
Glenn Bech <glenn.bech@gmail.com>
2018-10-14 14:54:23 +0200

    hello

--- src/main/java/com/example/herokupipeexample/DemoController.java
+++ src/main/java/com/example/herokupipeexample/DemoController.java
@@ -12,7 +12,7 @@ public class DemoController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="Worldz") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Worldzjsdhcjshcjsd") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
